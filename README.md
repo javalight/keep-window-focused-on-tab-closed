521f8f1d89333fe832fc1b24e7deb1237524120e
javalight <lightprogramming@gmail.com>
2023-02-10 11:21:07 -0500

    Update

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+<p align="center">
+  <img src="icon.png" width="200" alt="accessibility text">
+</p>
+
+
 # Keep Window Focused On Tab Closed
 Sometimes when closing a tab chrome will randomly give focus to a different window, this extension fixs that by refocusing on the last window the tab was closed in. 
 
--- backgroundTabs.js
+++ backgroundTabs.js
@@ -1,5 +1,4 @@
 chrome.tabs.onRemoved.addListener((info, removeInfo) => onTabRemoved(info, removeInfo));
-// chrome.tabs.onActivated.addListener((info) => onAc(info))
 
 async function getCurrentTab() {
   let queryOptions = { active: true, currentWindow: true };
@@ -13,18 +12,10 @@ async function onTabRemoved(e, info) {
   try {
     windowClosedId = info.windowId
     windowClosed = true
-    chrome.windows.update(windowClosedId, { focused: true, drawAttention: true })
+
+    await chrome.windows.update(windowClosedId, { focused: true, drawAttention: true })
   } catch (error) {
     console.log(error)
   }
 }
 
-// async function onTabCloseUnfocus(current){
-//   if(windowClosed){
-//     windowClosed = false
-//     let currentTab = await getCurrentTab()
-//     if (currentTab.windowId != windowClosedId){
-//       chrome.windows.update(windowClosedId, {focused: true, drawAttention:true})
-//     }
-//   }
-// }
